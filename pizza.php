e1ac024f5b50872f984333cdb4e9f34cfc6b7f9e
nethkuna <456247.clot@fje.edu>
2025-02-12 17:18:07 +0000

    Commit 4.5 de l’aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -1,33 +1,27 @@
-<!DOCTYPE html>
-<html lang="ca">
-<head>
-    <meta charset="UTF-8">
-    <title>FES LA TEVA PIZZA</title>
-</head>
-<a href="pizza.html">Retorna al formulari de selecció d’ingredients</a>
-<body>
-    <?php
-    $preu = 8;
-    $IVA = 21;
-
-    if (empty($_GET['ingr'])) {
-        $pvp = ((100 + $IVA) / 100) * $preu;
-        echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
-        # exit(0);
-    } else {
-        $pvp = ((100 + $IVA) / 100) * ($preu + count($_GET['ingr']));
-        echo "Has demanat la pizza bàsica amb:<br>";
-        foreach ($_GET['ingr'] as $ingredient) {
-            if ($ingredient) {
-                echo "* $ingredient<br>";
-            }
-        }
-        echo "El preu és $pvp € (IVA inclòs)<br>";
-        # exit(0);
-    }
-
-    echo "<br>";
-    echo "Autor: jasofe<br>";
-    ?>
-</body>
+<html>
+     <head>
+          <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+          <title>FES LA TEVA PIZZA</title>
+     </head>
+     <body>
+     <?php
+          $preu = 8;
+          $IVA = 21;
+          if (empty($_GET['ingr'])) {
+               $pvp = ((100 + $IVA) / 100) * $preu;
+               echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
+               // exit(0);
+          } else {
+               $pvp = ((100 + $IVA) / 100) * ($preu + count($_GET['ingr']));
+               echo "Has demanat la pizza bàsica amb:<br>";
+               foreach ($_GET['ingr'] as $ingredient) {
+                    echo "* $ingredient<br>";
+               }
+               echo "El preu és $pvp € (IVA inclòs)<br>";
+               // exit(0);
+          }
+          echo "<br>";
+          echo "Autor: jasofe<br>";
+     ?>
+     </body>
 </html>
